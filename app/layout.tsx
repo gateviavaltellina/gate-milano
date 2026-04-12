762a1c198e807f25e42428cc8cf5ac560b0bcbb9
George <george@192.168.1.60>
2026-04-12 20:15:27 +0200

    add vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import { Bebas_Neue } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-gate-black text-gate-white">
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
